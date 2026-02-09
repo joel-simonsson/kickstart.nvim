6c13c19ee95565bb6490e35f948332579b406f7e
Joel Simonsson <joel.simonsson@edpconsult.se>
2026-02-09 09:26:59 +0100

    add relative number config

--- init.lua
+++ init.lua
@@ -170,8 +170,9 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.list = true
+--vim.opt.list = true
 vim.opt.listchars = { tab = '-> ', space = '.' }
+vim.opt.realtivenumber = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
